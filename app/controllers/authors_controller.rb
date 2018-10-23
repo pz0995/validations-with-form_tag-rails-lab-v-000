82aa56a992a80b6802921ffd57af405402663d67
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-23 14:32:59 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -16,7 +16,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new
-    if @author.new(author_params)
+    if @author.update(author_params)
       redirect_to author_path(@author)
     else
       render :new
