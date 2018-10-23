d15702d61916447f6b5d1d613ba225d67d721ec8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-23 15:33:41 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+    #@post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
